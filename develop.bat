98e571a4e824ddf62f4b7c9602629a17363667c6
oshima <ki13sd6p@gmail.com>
2020-05-12 15:17:38 +0900

    commitMessage

--- develop.bat
+++ develop.bat
@@ -1,15 +1,16 @@
 echo off
-git branch >> log.txt
-set /p branchName= < branch.txt
+
+git branch --contains >> log.txt
+set /p branchName= <log.txt
+del log.txt
+
 set commitMessage=
 echo [    please commit message.   ]
 echo [    don't include ".         ]
 set /p commitMessage="---->"
-echo %branchName%
-echo %commitMessage%
+
+echo on
 git add .
 git commit -a -m "%commitMessage"
-for /f "delims=* tokens=1,2" %%a in (log.txt) do git push origin %%a
-del log.txt
-echo on
-echo completed.
+git push origin %branchName:~2%
+Pause
