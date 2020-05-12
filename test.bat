21eb01de40c84a7f58f16f3fdcd4ad2f0487be0e
oshima <ki13sd6p@gmail.com>
2020-05-12 13:35:09 +0900

    test test

--- test.bat
+++ test.bat
@@ -1,6 +1,14 @@
+
+echo off
+set commitMessage=
+echo [    please commit message    ]
+echo [    do not include "         ]
+set /P commitMessage="> "
 set branch= < branch.txt
 set imgName=test2
+
+echo on
 git add .
-git commit -a -m "test1 %imgName%"
+git commit -a -m "%commitMessage%"
 git push origin %branch%
 Pause
