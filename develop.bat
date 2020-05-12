bf07b6912fe7b4120e08e4e3c3bce6a32fa3ab90
oshima <ki13sd6p@gmail.com>
2020-05-12 14:00:32 +0900

    commitMessage

--- develop.bat
+++ develop.bat
@@ -1,14 +1,14 @@
-
 echo off
+set /p branchName= < branch.txt
 set commitMessage=
-echo [    please commit message    ]
-echo [    do not include "         ]
-set /P commitMessage="> "
-set branch= < branch.txt
-set imgName=test2
+echo [    please commit message.   ]
+echo [    don't include ".         ]
+set /p commitMessage="---->"
+echo %branchName%
+echo %commitMessage%
 
 echo on
 git add .
-git commit -a -m "%commitMessage%"
-git push origin %branch%
+git commit -a -m "%commitMessage"
+git push origin %branchName%
 Pause
