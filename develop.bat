3c0633804f89967d844b3d8002c0ef1dad879e28
oshima <ki13sd6p@gmail.com>
2020-06-18 21:07:35 +0900

    sango test img upload

---
+++ develop.bat
@@ -0,0 +1,16 @@
+echo off
+git branch --contains >> qwertyuioplkhgfdsazxcvbnm.txt
+set /p branchName= <qwertyuioplkhgfdsazxcvbnm.txt
+del qwertyuioplkhgfdsazxcvbnm.txt
+
+set commitMessage=
+echo [    please commit message.   ]
+echo [    don't include ".         ]
+set /p commitMessage="---->"
+
+echo on
+git add .
+git reset head %~dp0%develop.txt
+git commit -a -m "%commitMessage%"
+git push origin %branchName:~2%
+Pause
